faa91f7d94f3fc31f29f8b515edf37a18925fcc1
christopher.lloyd2 <Christopher.Lloyd2@ibm.com>
2020-03-09 12:35:44 +0000

    added examples and some explanation in the readme

---
+++ example/module.py
@@ -0,0 +1,10 @@
+# this is a module
+
+# functions should do one thing only
+def pow(number, exponent):
+    """This function returns the the number to the power of the exponent."""
+    return number**exponent
+
+def add(number1, number2):
+    """This function returns number1 + number2"""
+    return number1 + number2
