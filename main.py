63d21f787413596f78c8a9be6c4a0a5f03979d56
billyrturner <billyrturner1@gmail.com>
2020-03-13 20:58:23 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
-help_message = """The following is a list of valid inputs:
+def main():
+    help_message = """The following is a list of valid inputs:
+    Help - display this message
+    Quit - exit the program
+    Restart - restarts the program from the beginning"""
 
-Help - display this message
-Quit - exit the program
-Restart - it does something?"""
+    welcome_message = "Welcome to the Stabbings search tool"
 
-welcome_message = "Welcome to the Stabbings search tool"
+    end_program = False
 
-end_program = False
+    print("\n" + welcome_message)
 
-print("\n" + welcome_message)
-print(help_message)
 
-while not end_program:
-    user_input = input(" > ")
-    user_input = user_input.lower()
-    print()
-    if user_input == "help":
-        print(help_message) 
-    elif user_input == "restart":
-        print("not sure what this is meant to do yet")
-    elif user_input == "quit":
-        end_program = True
-    else:
-        print("That is not a valid command. Type 'help' for a list of command.")
+    while not end_program:
+        user_input = input(" > ")
+        user_input = user_input.lower()
+        print()
+        if user_input == "help":
+            print(help_message) 
+        elif user_input == "restart":
+            main()
+        elif user_input == "quit":
+            end_program = True
+        else:
+            print("That is not a valid command. Type 'help' for a list of command.")
+
+if __name__ == '__main__':
+    main()
+
