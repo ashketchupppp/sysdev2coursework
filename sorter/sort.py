b6d10b1dd1ddb864ab7b7dd5c5aa40a4ab7289d8
billyrturner <billyrturner1@gmail.com>
2020-03-23 11:05:13 +0000

    Update sort.py

--- sorter/sort.py
+++ sorter/sort.py
@@ -1,14 +1,8 @@
 from datetime import datetime
-#Below is example data Ive used to help test this module
-unsortedDict = [
-      {'Distance (km)': 2.1, 'Date': '12/12/15 15:01', 'Crime Category': 'Other theft'}, 
-      {'Distance (km)': 0.6, 'Date': '1/1/14 18:21', 'Crime Category': 'Anti-social behaviour'}, 
-      {'Distance (km)': 1.5, 'Date': '10/5/14 22:41', 'Crime Category': 'Other crime'}, 
-      {'Distance (km)': 4.4, 'Date': '1/4/14 18:20', 'Crime Category': 'Shoplifting'}, 
-      {'Distance (km)': 3.2, 'Date': '2/2/14 23:05', 'Crime Category': 'Public order'}, 
-      {'Distance (km)': 1.7, 'Date': '9/11/13 9:02', 'Crime Category': 'Vehicle crime'}, 
-      {'Distance (km)': 4.0, 'Date': '30/7/14 11:28', 'Crime Category': 'Other crime'}
-    ]
+
+""" Please note I have used unsortedDict as the name of the unsorted dictionary
+    The dictionary should be changed to the appropriate name outputted from
+    the filter module"""
 
 def sortDate(reverse):
     """ When reverse=True, data will be sorted by most recent first"""
@@ -24,6 +18,3 @@ def sortDistCrime(type, reverse):
     return sortedDict
 
 
-print(sortDate(True))
-print()
-print(sortDistCrime('Distance (km)', True))
