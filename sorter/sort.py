552b8acaf0f5de0d9d59a0db39ed951329c4f728
billyrturner <billyrturner1@gmail.com>
2020-03-23 12:31:00 +0000

    Update sort.py

--- sorter/sort.py
+++ sorter/sort.py
@@ -1,19 +1,15 @@
 from datetime import datetime
 
-""" Please note I have used unsortedDict as the name of the unsorted dictionary
-    The dictionary should be changed to the appropriate name outputted from
-    the filter module"""
 
-def sortDate(reverse):
+def sortDate(unsortedDict, reverse):
     """ When reverse=True, data will be sorted by most recent first"""
     sortedDict = sorted(unsortedDict, key=lambda x: datetime.strptime(x['Date'], '%d/%m/%y %H:%M'), reverse=reverse)
     return sortedDict
 
 
-def sortDistCrime(type, reverse):
+def sortDistCrime(unsortedDict, type, reverse):
     """ When reverse=True, data will be sorted by reverse alphabetical order for Crime Category
-        or by crime furthest away from Postcode first  
-    """
+        or by crime furthest away from Postcode first """
     sortedDict = sorted(unsortedDict, key = lambda i: i[type], reverse=reverse)
     return sortedDict
 
