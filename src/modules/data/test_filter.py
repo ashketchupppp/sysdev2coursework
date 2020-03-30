1468a41e7a3881e6249ee8300d5e3c5a59a2b046
billyrturner <billyrturner1@gmail.com>
2020-03-30 16:53:56 +0100

    Update test_filter.py

--- src/modules/data/test_filter.py
+++ src/modules/data/test_filter.py
@@ -1,9 +1,24 @@
 import unittest
+from filter import *
 
-class TestFilter(unittest.TestCase):
-    def test_example(self):
-        """this is an example test. please include docstrings in your functions"""
-        self.assertEqual(2, 1+1)
+"""Here is an example dictionary, used to test the sort functions work correctly in the sort module"""
+dicts = [
+    {'first': 'Jim', 'distance': 2.8}, 
+    {'first': 'James','distance': 7.5},
+    {'first': 'Claire','distance': 1.3},
+    {'first': 'Sam','distance': 0.1},
+    {'first': 'Olivia','distance': 12.5},
+    {'first': 'Christian','distance': 4.9}
+    ]
+
+class TestModuleFilter(unittest.TestCase):
+    def test_filter_function(self):
+        """ Checks sort function sorts alphabetically descending. """
+        expectedOutput = [{'first': 'Jim', 'distance': 2.8}, {'first': 'Claire', 'distance': 1.3}, {'first': 'Sam', 'distance': 0.1}, {'first': 'Christian', 'distance': 4.9}]
+        
+    
+        actualOutput = filterData(dicts, 'distance', 5)
+        self.assertEqual(actualOutput, expectedOutput)
 
 # this is required to be able to run the tests
 if __name__ == '__main__':
