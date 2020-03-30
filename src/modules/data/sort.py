de0accac760b05d21ede2959ecebb713890e77bc
christopher.lloyd2 <Christopher.Lloyd2@ibm.com>
2020-03-30 12:05:28 +0100

    moved sorter updates to new data module

--- src/modules/data/sort.py
+++ src/modules/data/sort.py
@@ -1,16 +1,12 @@
 from datetime import datetime
 
-
-def sortDate(unsortedDict, reverse):
-    """ When reverse=True, data will be sorted by most recent first"""
-    sortedDict = sorted(unsortedDict, key=lambda x: datetime.strptime(x['Date'], '%d/%m/%y %H:%M'), reverse=reverse)
-    return sortedDict
-
-
-def sortDistCrime(unsortedDict, type, reverse):
-    """ When reverse=True, data will be sorted by reverse alphabetical order for Crime Category
-        or by crime furthest away from Postcode first """
-    sortedDict = sorted(unsortedDict, key = lambda i: i[type], reverse=reverse)
-    return sortedDict
-
-
+# if you are sorting a date then just pass a value for dateFormat
+# if you are trying to use this function I would recommend looking at the tests
+# to see how to use it
+def listOfDictSort(unsortedListOfDict, key, reverse=False, dateFormat=""):
+    """ Sorts an unsorted list of dictionaries by a passed key value. """
+    if dateFormat != "":
+        sortedListOfDict = sorted(unsortedListOfDict, key=lambda x: datetime.strptime(x[key], dateFormat), reverse=reverse)
+    else:
+        sortedListOfDict = sorted(unsortedListOfDict, key = lambda i: i[key], reverse=reverse)
+    return sortedListOfDict
--- src/modules/data/sort_test.py
+++ src/modules/data/sort_test.py
@@ -3,62 +3,103 @@ import sort
 
 """Here is an example dictionary, used to test the sort functions work correctly in the sort module"""
 unsortedDict = [
-    {'Distance (km)': 2.1, 'Date': '12/12/15 15:01', 'Crime Category': 'Other theft'}, 
-    {'Distance (km)': 0.6, 'Date': '1/1/14 18:21', 'Crime Category': 'Anti-social behaviour'}, 
-    {'Distance (km)': 1.5, 'Date': '10/5/14 22:41', 'Crime Category': 'Other crime'}, 
-    {'Distance (km)': 4.4, 'Date': '1/4/14 18:20', 'Crime Category': 'Shoplifting'}, 
-    {'Distance (km)': 3.2, 'Date': '2/2/14 23:05', 'Crime Category': 'Public order'}, 
-    {'Distance (km)': 1.7, 'Date': '9/11/13 9:02', 'Crime Category': 'Vehicle crime'}, 
-    {'Distance (km)': 4.0, 'Date': '30/7/14 11:28', 'Crime Category': 'Other crime'}
+    {'Distance (km)': 2.1, 'Date': '2015-12', 'Crime Category': 'Other theft'}, 
+    {'Distance (km)': 0.6, 'Date': '2014-1', 'Crime Category': 'Anti-social behaviour'}, 
+    {'Distance (km)': 1.5, 'Date': '2014-5', 'Crime Category': 'Other crime'}, 
+    {'Distance (km)': 4.4, 'Date': '2014-4', 'Crime Category': 'Shoplifting'}, 
+    {'Distance (km)': 3.2, 'Date': '2014-2', 'Crime Category': 'Public order'}, 
+    {'Distance (km)': 1.7, 'Date': '2013-11', 'Crime Category': 'Vehicle crime'}, 
+    {'Distance (km)': 4.0, 'Date': '2014-7', 'Crime Category': 'Other crime'}
     ]
 
 class TestModuleSorter(unittest.TestCase):
-    def test_crime_category_sort(self):
-        """Checks sort function accurately sorts alphabetically by Crime Category"""
-        sortedDict = []
-        expectedOutput = [{'Distance (km)': 0.6, 'Date': '1/1/14 18:21', 'Crime Category': 'Anti-social behaviour'}, {'Distance (km)': 1.5, 'Date': '10/5/14 22:41', 'Crime Category': 'Other crime'}, {'Distance (km)': 4.0, 'Date': '30/7/14 11:28', 'Crime Category': 'Other crime'}, {'Distance (km)': 2.1, 'Date': '12/12/15 15:01', 'Crime Category': 'Other theft'}, {'Distance (km)': 3.2, 'Date': '2/2/14 23:05', 'Crime Category': 'Public order'}, {'Distance (km)': 4.4, 'Date': '1/4/14 18:20', 'Crime Category': 'Shoplifting'}, {'Distance (km)': 1.7, 'Date': '9/11/13 9:02', 'Crime Category': 'Vehicle crime'}]
+    def test_alphabetic_sort_desc(self):
+        """ Checks sort function sorts alphabetically descending. """
+        expectedOutput = [{'Distance (km)': 0.6, 'Date': '2014-1', 'Crime Category': 'Anti-social behaviour'},
+                          {'Distance (km)': 1.5, 'Date': '2014-5', 'Crime Category': 'Other crime'},
+                          {'Distance (km)': 4.0, 'Date': '2014-7', 'Crime Category': 'Other crime'},
+                          {'Distance (km)': 2.1, 'Date': '2015-12', 'Crime Category': 'Other theft'},
+                          {'Distance (km)': 3.2, 'Date': '2014-2', 'Crime Category': 'Public order'},
+                          {'Distance (km)': 4.4, 'Date': '2014-4', 'Crime Category': 'Shoplifting'},
+                          {'Distance (km)': 1.7, 'Date': '2013-11', 'Crime Category': 'Vehicle crime'}]
         
-        actualOutput = sort.sortDistCrime(unsortedDict, 'Crime Category', False)
+    
+        actualOutput = sort.listOfDictSort(unsortedListOfDict=unsortedDict, key='Crime Category', reverse=False)
         self.assertEqual(actualOutput, expectedOutput)
 
-    def test_crime_category_sort_reverse(self):
-        """Checks sort function accurately sorts reverse alphabetically by Crime Category"""
-        sortedDict = []
-        expectedOutput = [{'Distance (km)': 1.7, 'Date': '9/11/13 9:02', 'Crime Category': 'Vehicle crime'}, {'Distance (km)': 4.4, 'Date': '1/4/14 18:20', 'Crime Category': 'Shoplifting'}, {'Distance (km)': 3.2, 'Date': '2/2/14 23:05', 'Crime Category': 'Public order'}, {'Distance (km)': 2.1, 'Date': '12/12/15 15:01', 'Crime Category': 'Other theft'}, {'Distance (km)': 1.5, 'Date': '10/5/14 22:41', 'Crime Category': 'Other crime'}, {'Distance (km)': 4.0, 'Date': '30/7/14 11:28', 'Crime Category': 'Other crime'}, {'Distance (km)': 0.6, 'Date': '1/1/14 18:21', 'Crime Category': 'Anti-social behaviour'}]
+    def test_alphabetic_sort_asc(self):
+        """ Checks sort function sorts alphabetically ascending. """
+        expectedOutput = [{'Distance (km)': 1.7, 'Date': '2013-11', 'Crime Category': 'Vehicle crime'},
+                          {'Distance (km)': 4.4, 'Date': '2014-4', 'Crime Category': 'Shoplifting'},
+                          {'Distance (km)': 3.2, 'Date': '2014-2', 'Crime Category': 'Public order'},
+                          {'Distance (km)': 2.1, 'Date': '2015-12', 'Crime Category': 'Other theft'},
+                          {'Distance (km)': 1.5, 'Date': '2014-5', 'Crime Category': 'Other crime'},
+                          {'Distance (km)': 4.0, 'Date': '2014-7', 'Crime Category': 'Other crime'},
+                          {'Distance (km)': 0.6, 'Date': '2014-1', 'Crime Category': 'Anti-social behaviour'}]
         
-        actualOutput = sort.sortDistCrime(unsortedDict, 'Crime Category', True)
+    
+        actualOutput = sort.listOfDictSort(unsortedListOfDict=unsortedDict, key='Crime Category', reverse=True)
         self.assertEqual(actualOutput, expectedOutput)
 
-    def test_distance_sort_nearest(self):
-        """Checks sort function accurately sorts by distance from postcode, nearest first"""
+    def test_number_sort_asc(self):
+        """ Checks sort function sorts numbers ascending. """
         sortedDict = []
-        expectedOutput = [{'Distance (km)': 0.6, 'Date': '1/1/14 18:21', 'Crime Category': 'Anti-social behaviour'}, {'Distance (km)': 1.5, 'Date': '10/5/14 22:41', 'Crime Category': 'Other crime'}, {'Distance (km)': 1.7, 'Date': '9/11/13 9:02', 'Crime Category': 'Vehicle crime'}, {'Distance (km)': 2.1, 'Date': '12/12/15 15:01', 'Crime Category': 'Other theft'}, {'Distance (km)': 3.2, 'Date': '2/2/14 23:05', 'Crime Category': 'Public order'}, {'Distance (km)': 4.0, 'Date': '30/7/14 11:28', 'Crime Category': 'Other crime'}, {'Distance (km)': 4.4, 'Date': '1/4/14 18:20', 'Crime Category': 'Shoplifting'}]
+        expectedOutput = [{'Distance (km)': 0.6, 'Date': '2014-1', 'Crime Category': 'Anti-social behaviour'},
+                          {'Distance (km)': 1.5, 'Date': '2014-5', 'Crime Category': 'Other crime'},
+                          {'Distance (km)': 1.7, 'Date': '2013-11', 'Crime Category': 'Vehicle crime'},
+                          {'Distance (km)': 2.1, 'Date': '2015-12', 'Crime Category': 'Other theft'},
+                          {'Distance (km)': 3.2, 'Date': '2014-2', 'Crime Category': 'Public order'},
+                          {'Distance (km)': 4.0, 'Date': '2014-7', 'Crime Category': 'Other crime'},
+                          {'Distance (km)': 4.4, 'Date': '2014-4', 'Crime Category': 'Shoplifting'}]
         
-        actualOutput = sort.sortDistCrime(unsortedDict, 'Distance (km)', False)
+    
+        actualOutput = sort.listOfDictSort(unsortedListOfDict=unsortedDict, key='Distance (km)', reverse=False)
         self.assertEqual(actualOutput, expectedOutput)
         
-    def test_distance_sort_furthest(self):
-        """Checks sort function accurately sorts by distance from postcode, farthest first"""
+    def test_number_sort_desc(self):
+        """ Checks sort function sorts numbers descending. """
         sortedDict = []
-        expectedOutput = [{'Distance (km)': 4.4, 'Date': '1/4/14 18:20', 'Crime Category': 'Shoplifting'}, {'Distance (km)': 4.0, 'Date': '30/7/14 11:28', 'Crime Category': 'Other crime'}, {'Distance (km)': 3.2, 'Date': '2/2/14 23:05', 'Crime Category': 'Public order'}, {'Distance (km)': 2.1, 'Date': '12/12/15 15:01', 'Crime Category': 'Other theft'}, {'Distance (km)': 1.7, 'Date': '9/11/13 9:02', 'Crime Category': 'Vehicle crime'}, {'Distance (km)': 1.5, 'Date': '10/5/14 22:41', 'Crime Category': 'Other crime'}, {'Distance (km)': 0.6, 'Date': '1/1/14 18:21', 'Crime Category': 'Anti-social behaviour'}]
+        expectedOutput = [{'Distance (km)': 4.4, 'Date': '2014-4', 'Crime Category': 'Shoplifting'},
+                          {'Distance (km)': 4.0, 'Date': '2014-7', 'Crime Category': 'Other crime'},
+                          {'Distance (km)': 3.2, 'Date': '2014-2', 'Crime Category': 'Public order'},
+                          {'Distance (km)': 2.1, 'Date': '2015-12', 'Crime Category': 'Other theft'},
+                          {'Distance (km)': 1.7, 'Date': '2013-11', 'Crime Category': 'Vehicle crime'},
+                          {'Distance (km)': 1.5, 'Date': '2014-5', 'Crime Category': 'Other crime'},
+                          {'Distance (km)': 0.6, 'Date': '2014-1', 'Crime Category': 'Anti-social behaviour'}]
         
-        actualOutput = sort.sortDistCrime(unsortedDict, 'Distance (km)', True)
+    
+        actualOutput = sort.listOfDictSort(unsortedListOfDict=unsortedDict, key='Distance (km)', reverse=True)
         self.assertEqual(actualOutput, expectedOutput)
     
     def test_date_sort_newest(self):
         """Checks sort function accurately sorts by date and time, most recent first"""
         sortedDict = []
-        expectedOutput = [{'Distance (km)': 2.1, 'Date': '12/12/15 15:01', 'Crime Category': 'Other theft'}, {'Distance (km)': 4.0, 'Date': '30/7/14 11:28', 'Crime Category': 'Other crime'}, {'Distance (km)': 1.5, 'Date': '10/5/14 22:41', 'Crime Category': 'Other crime'}, {'Distance (km)': 4.4, 'Date': '1/4/14 18:20', 'Crime Category': 'Shoplifting'}, {'Distance (km)': 3.2, 'Date': '2/2/14 23:05', 'Crime Category': 'Public order'}, {'Distance (km)': 0.6, 'Date': '1/1/14 18:21', 'Crime Category': 'Anti-social behaviour'}, {'Distance (km)': 1.7, 'Date': '9/11/13 9:02', 'Crime Category': 'Vehicle crime'}]
+#2015-12 
+        expectedOutput = [{'Distance (km)': 2.1, 'Date': '2015-12', 'Crime Category': 'Other theft'},
+                          {'Distance (km)': 4.0, 'Date': '2014-7', 'Crime Category': 'Other crime'},
+                          {'Distance (km)': 1.5, 'Date': '2014-5', 'Crime Category': 'Other crime'},
+                          {'Distance (km)': 4.4, 'Date': '2014-4', 'Crime Category': 'Shoplifting'},
+                          {'Distance (km)': 3.2, 'Date': '2014-2', 'Crime Category': 'Public order'},
+                          {'Distance (km)': 0.6, 'Date': '2014-1', 'Crime Category': 'Anti-social behaviour'},
+                          {'Distance (km)': 1.7, 'Date': '2013-11', 'Crime Category': 'Vehicle crime'}]
         
-        actualOutput = sort.sortDate(unsortedDict, True)
+    
+        actualOutput = sort.listOfDictSort(unsortedDict, 'Date', reverse=True, dateFormat='%Y-%m')
         self.assertEqual(actualOutput, expectedOutput)
       
     def test_distance_sort_oldest(self):
         """Checks sort function accurately sorts by date and time, oldest first"""
         sortedDict = []
-        expectedOutput = [{'Distance (km)': 1.7, 'Date': '9/11/13 9:02', 'Crime Category': 'Vehicle crime'}, {'Distance (km)': 0.6, 'Date': '1/1/14 18:21', 'Crime Category': 'Anti-social behaviour'}, {'Distance (km)': 3.2, 'Date': '2/2/14 23:05', 'Crime Category': 'Public order'}, {'Distance (km)': 4.4, 'Date': '1/4/14 18:20', 'Crime Category': 'Shoplifting'}, {'Distance (km)': 1.5, 'Date': '10/5/14 22:41', 'Crime Category': 'Other crime'}, {'Distance (km)': 4.0, 'Date': '30/7/14 11:28', 'Crime Category': 'Other crime'}, {'Distance (km)': 2.1, 'Date': '12/12/15 15:01', 'Crime Category': 'Other theft'}]
+        expectedOutput = [{'Distance (km)': 1.7, 'Date': '2013-11', 'Crime Category': 'Vehicle crime'},
+                          {'Distance (km)': 0.6, 'Date': '2014-1', 'Crime Category': 'Anti-social behaviour'},
+                          {'Distance (km)': 3.2, 'Date': '2014-2', 'Crime Category': 'Public order'},
+                          {'Distance (km)': 4.4, 'Date': '2014-4', 'Crime Category': 'Shoplifting'},
+                          {'Distance (km)': 1.5, 'Date': '2014-5', 'Crime Category': 'Other crime'},
+                          {'Distance (km)': 4.0, 'Date': '2014-7', 'Crime Category': 'Other crime'},
+                          {'Distance (km)': 2.1, 'Date': '2015-12', 'Crime Category': 'Other theft'}]
         
-        actualOutput = sort.sortDate(unsortedDict, False)
+    
+        actualOutput = sort.listOfDictSort(unsortedDict, 'Date', reverse=False, dateFormat='%Y-%m')
         self.assertEqual(actualOutput, expectedOutput)
 
 # this is required to be able to run the tests
