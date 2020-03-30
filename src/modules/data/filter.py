a7eb23f241dbffc1094b730685a4ea7d710e06b7
billyrturner <billyrturner1@gmail.com>
2020-03-30 16:40:13 +0100

    Update filter.py

--- src/modules/data/filter.py
+++ src/modules/data/filter.py
@@ -1,6 +1,6 @@
-def filter(dictList, radius):
+def filterData(dictList, key, radius):
     """Function to filter the given data using the given values."""
-    filteredDict = [d for d in dictList if d['distance'] <= radius]
+    filteredDict = [d for d in dictList if d[key] <= radius]
     return filteredDict
     # this should use the distance between module to figure out if a coord is within the radius or not 
     # read the git issue to see exactly what this needs to do im not typing it out again
