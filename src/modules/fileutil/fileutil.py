e95038e513fa8281835f309515f6472fb372e512
christopher.lloyd2 <Christopher.Lloyd2@ibm.com>
2020-04-05 13:39:13 +0100

    updated file search and tests

--- src/modules/fileutil/fileutil.py
+++ src/modules/fileutil/fileutil.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 def recursive_search(directory, wildcard="*"):
     """Recursively search a directory for files matching the given wildcard."""
@@ -6,3 +7,16 @@ def recursive_search(directory, wildcard="*"):
     for path in Path(directory).rglob(wildcard):
         results.append(path) 
     return results
+
+def find_files(dir, filetype=''):
+    """ Recursively searches a directory for files matching the filetype.
+        Leave empty to get a list of all files.
+        Each Result item has the full path and just the filename in each result item."""
+    filelist = []
+    for root, dirs, files in os.walk(dir):
+        if len(files) > 0:
+            for f in files: 
+                if f[-3:] == filetype or filetype == '':
+                    filelist.append([root + '/' + f, f])
+    return filelist
+    
