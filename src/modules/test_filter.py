f5e40dd509d6f2a12e0c2be2109376956e90e5fe
cuthbert1221 <cuthbert1221@gmail.com>
2020-04-05 12:17:31 +0100

    Update test_filter.py

--- src/modules/test_filter.py
+++ src/modules/test_filter.py
@@ -29,7 +29,7 @@ class TestFilter(unittest.TestCase):
     def test_radius(self):
         """Testing that the filter returns empty list when radius is 0"""
         dictList = get_dict_list()
-        postcodelatlng = [50.827974, -4.543798]
+        postcodelatlng = [50.827974, -4.543799]
         radius = 0
         actualOutput = filterData(dictList, postcodelatlng, radius)
         expectedOutput = []
@@ -37,8 +37,8 @@ class TestFilter(unittest.TestCase):
     
     def test_working_single(self):
         """Testing that the filter returns the expected results with only one input"""
-        dictList = get_dict_list()
-        dictList = [{'Crime ID': '1', 'Longitude': '-4.543798', 'Latitude': '50.830723'}]
+        test_data = get_dict_list()
+        dictList = [test_data[0]]
         postcodelatlng = [51.830723, -4.543798]
         radius = 1000
         actualOutput = filterData(dictList, postcodelatlng, radius)
@@ -47,6 +47,7 @@ class TestFilter(unittest.TestCase):
         
     def test_empty_input(self):
         """Testing that the filter returns the expected results with only one input"""
+        #Testing Empty input
         dictList = []
         postcodelatlng = [50.830723, -4.543798]
         radius = 100
@@ -56,6 +57,7 @@ class TestFilter(unittest.TestCase):
     
     def test_same_coord(self):
         """Testing one result returned if radius is 0 and input empty"""
+        #Testing Empty input
         dictList = []
         postcodelatlng = [51.830723, -4.543798]
         radius = 0
@@ -65,8 +67,8 @@ class TestFilter(unittest.TestCase):
         
     def test_not_list(self):
         """Testing that empty list retunred if input is not a list - not error"""
-        dictList = get_dict_list()
-        dictList = {'Crime ID': '1', 'Longitude': '-4.543798', 'Latitude': '50.830723'}
+        test_data = get_dict_list()
+        dictList = test_data[0]
         postcodelatlng = [50.830723, -4.543798]
         radius = 1000
         actualOutput = filterData(dictList, postcodelatlng, radius)
