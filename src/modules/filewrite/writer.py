343de419cf88540482436b2aed29a602613d00c7
Jacob Tidridge <jtidridge@css.int>
2020-04-06 13:54:26 +0100

    Update comments
    filter
    writer

--- src/modules/filewrite/writer.py
+++ src/modules/filewrite/writer.py
@@ -3,22 +3,23 @@ import os
 
 def dict_to_csv(filepath, content):
     """Writes contents of a dictionary to a .csv file"""
-    #Check if the file is a csv file, return if not
+    #Check if filepath is csv file, return if not
     if filepath[-4:] != ".csv":
         print("Not csv file")
         return [-1]
-    #Checking content type, retun if invalid
+    #Checking content type, return if invalid
     if not isinstance(content,(list,dict)):
         print("Not List or Dictionary")
         return [-1]
     #Create list of keys, for headings
     keys = content[0].keys()
-    #Catch exceptions and return -1 -error
     try:
+        #Write header + rows to file
         with open(filepath, 'w', newline="") as csv_file:
             dict_writer = csv.DictWriter(csv_file, keys)  
-            dict_writer.writeheader() #Add the csv Headers
-            dict_writer.writerows(content) #write content on each line
-        return filepath #return the written file
+            dict_writer.writeheader() 
+            dict_writer.writerows(content)
+        #return the written file
+        return filepath 
     except Exception:
         return [-1]
